bea12f970d2b4d98314a742f679a7eec6471d77e
mayank-tif <mayank@tiftechnologies.com>
2025-10-13 10:05:33 +0530

    middleware

--- FWC2025/settings.py
+++ FWC2025/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'fwc.middleware.SessionCheckByMiddleware',
+    'fwc.middleware.DomainRedirectMiddleware',
 ]
 
 
